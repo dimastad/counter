6102ecc9bc2fa84b40822fef811431b4ded69c77
Dmitrii Portnov <dimastad29@gmail.com>
2019-06-03 23:45:19 +0400

    added localStorage

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -4,7 +4,7 @@ import style from '../styles/Counter.css';
 
 export default function Counter() {
 
-    const [count, setCount] = useState(0);
+    const [count, setCount] = useState(localStorage.getItem('value') !== null ? localStorage.getItem('value') : 0);
 
     return (
         <div className={style.block}>
@@ -20,6 +20,9 @@ export default function Counter() {
             </button>
             <button className={style.button} onClick={() => setCount(count * 0)}>
                 Обнулить
+            </button>
+            <button className={style.button} onClick={() => localStorage.setItem('value', count)}>
+                сохранить
             </button>
         </div>
     );
